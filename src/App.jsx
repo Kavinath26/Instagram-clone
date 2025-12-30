c67c3d4f48ab85ac3e44d30a12d81547a3808bf5
Kavinath26 <gkkavinath26042005@gmail.com>
2025-12-30 13:49:10 +0530

    side bar done

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,13 @@
-function App(){
-  return(
-    <div></div>
+import React from 'react'
+import Sidebar from './Sidebar'
+
+function App() {
+  return (
+    <div className='d-flex vh-100'>
+      <div className='w-20'><Sidebar/></div>
+      <div className='w-50 bg-info'>feed</div>
+      <div className='w-30'>suggestion</div>
+    </div>
   )
 }
 
