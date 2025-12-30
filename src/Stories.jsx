acca6c4f40ce51f292a6034191000fd0a1c66dc5
Kavinath26 <gkkavinath26042005@gmail.com>
2025-12-30 20:21:54 +0530

    completed 80%

--- src/Stories.jsx
+++ src/Stories.jsx
@@ -1,8 +1,37 @@
-import React from 'react'
+import React, { useState,useEffect } from 'react'
+import { useNavigate } from 'react-router-dom'
 
 function Stories() {
+
+  const [stories,setStories]=useState([])
+
+  const navigate=useNavigate()
+
+  let tot=0;
+
+  useEffect(()=>{
+        fetch('http://localhost:8000/story')
+        .then((data)=>data.json())
+        .then((data=>setStories(data)))
+        .catch(err=>console.log(err))
+    },[])
+ 
   return (
-    <div className='story bg-info'>Stories</div>
+    <div className="d-flex gap-3 story">
+      <div className='d-none'>{tot=stories.length}</div>
+      {stories.length > 0 ?(
+            stories.map((story)=>(
+                <div key={story.id}>
+                  <div className="gradient_border" onClick={()=>{navigate(`/story/${story.id}/${tot}`)}}>
+                  <img src={story.user.profile_pic} alt="" className='story-dp rounded-circle' />
+                  </div>
+                  <p className='text-truncate' style={{width:"50px", textAlign:"center"}}>{story.user.username}</p>
+                </div>
+            ))
+        ):(
+           <div>Loading</div>
+        )}
+    </div>
   )
 }
 
--- src/Suggestion.jsx
+++ src/Suggestion.jsx
@@ -1,8 +1,61 @@
-import React from 'react'
+import React, { useState,useEffect } from 'react'
 
 function Suggestion() {
+
+  const [profile,setProfile]=useState(null)
+  const [suggestions,setSuggestions]=useState([])
+
+  useEffect(()=>{
+          fetch('http://localhost:8000/profile')
+          .then((data)=>data.json())
+          .then((data=>setProfile(data)))
+          .catch(err=>console.log(err))
+      },[])
+
+  useEffect(()=>{
+          fetch('http://localhost:8000/Suggestion')
+          .then((data)=>data.json())
+          .then((data=>setSuggestions(data)))
+          .catch(err=>console.log(err))
+      },[])
+
   return (
-    <div>Suggestion</div>
+    <div>
+      <div className='suggestion w-75 m-4'>
+      {profile ?(
+        <div className='d-flex'>
+          <img className='dp rounded-circle' src={profile.profile_pic} alt='profile pic'/>
+          <h5 className='name'>{profile.username}</h5>
+          <small className='ms-auto text-primary'>Switch</small>
+        </div>):(
+        <div>Loading page</div>
+        )}
+        <div className='d-flex'>
+          <p>Suggested for you</p>
+          <b className='ms-auto'>See All</b>
+        </div>
+        <div>
+           {suggestions.length > 0 ?(
+           <div>
+            {suggestions.map((suggestion)=>
+                <div key={suggestion.id} className='my-2'>
+                    <div className='d-flex'>
+                        <img className='dp rounded-circle' src={suggestion.profile_pic} alt='profile pic'/>
+                        <h5 className='name'>{suggestion.username}</h5>
+                        <small className='text-primary ms-auto'>Follow</small>
+                    </div>
+                </div>
+            )}
+           </div>
+        ):(
+           <div>Loading</div>
+        )
+          
+        }
+        </div>
+
+      </div>
+    </div>
   )
 }
 
